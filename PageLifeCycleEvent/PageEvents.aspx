81ba28336dbcaed3a0f9946753b19c32cfefa162
Arvind <arvindmepani@gmail.com>
2018-06-07 11:12:35 +0530

    Initial commit for Page life cycle in webform

---
+++ PageLifeCycleEvent/PageEvents.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PageEvents.aspx.cs" Inherits="PageLifeCycleEvent.PageEvents" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:DropDownList ID="ddlCountry" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged"
+                OnInit="ddlCountry_Init" OnLoad="ddlCountry_Load" OnPreRender="ddlCountry_PreRender">
+                <asp:ListItem>India</asp:ListItem>
+                <asp:ListItem>USA</asp:ListItem>
+                <asp:ListItem>UK</asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="btnSave" runat="server" OnClick="btnSave_Click" OnInit="btnSave_Init" OnLoad="btnSave_Load" OnPreRender="btnSave_PreRender"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ PageLifeCycleEvent/PageEvents.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageLifeCycleEvent
+{
+    public partial class PageEvents : System.Web.UI.Page
+    {
+        List<string> lifeCycleEvent = new List<string>();
+        public PageEvents()
+        {
+            lifeCycleEvent.Add("Constructor");
+            Debug.WriteLine("Constructor");
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Page_Load");
+            lifeCycleEvent.Add("Page_Load");
+        }
+
+        protected override void OnPreInit(EventArgs e)
+        {
+            Debug.WriteLine("OnPreInit");
+            base.OnPreInit(e);
+            lifeCycleEvent.Add("OnPreInit");
+        }
+
+
+        protected override void OnInit(EventArgs e)
+        {
+            Debug.WriteLine("OnInit");
+            base.OnInit(e);
+            lifeCycleEvent.Add("OnInit");
+        }
+        protected override void OnInitComplete(EventArgs e)
+        {
+            Debug.WriteLine("OnInitComplete");
+            base.OnInitComplete(e);
+            lifeCycleEvent.Add("OnInitComplete");
+        }
+        protected override void OnPreLoad(EventArgs e)
+        {
+            Debug.WriteLine("OnPreLoad");
+            base.OnPreLoad(e);
+            lifeCycleEvent.Add("OnPreLoad");
+        }
+        protected override void OnLoadComplete(EventArgs e)
+        {
+            Debug.WriteLine("OnLoadComplete");
+            base.OnLoadComplete(e);
+            lifeCycleEvent.Add("OnLoadComplete");
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            Debug.WriteLine("OnPreRender");
+            base.OnPreRender(e);
+            lifeCycleEvent.Add("OnPreRender");
+        }
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            Debug.WriteLine("OnPreRenderComplete");
+            base.OnPreRenderComplete(e);
+            lifeCycleEvent.Add("OnPreRenderComplete");
+        }
+
+        protected override void OnSaveStateComplete(EventArgs e)
+        {
+            Debug.WriteLine("OnSaveStateComplete");
+            base.OnSaveStateComplete(e);
+            lifeCycleEvent.Add("OnSaveStateComplete");
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+            Debug.WriteLine("Render");
+            lifeCycleEvent.Add("Render");
+            writer.WriteLine("<h3>Page life cycle events</h3>");
+            writer.WriteLine("<ul>");
+            foreach (var item in lifeCycleEvent)
+            {
+                writer.WriteLine("<li>" + item + "</li>");
+            }
+            writer.WriteLine("</ul>");
+
+            base.Render(writer);
+        }
+        protected override void OnUnload(EventArgs e)
+        {
+            Debug.WriteLine("OnUnload");
+            lifeCycleEvent.Add("OnUnload");
+            base.OnUnload(e);
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ddlCountry_SelectedIndexChanged");
+            lifeCycleEvent.Add("ddlCountry_SelectedIndexChanged");
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("btnSave_Click");
+            lifeCycleEvent.Add("btnSave_Click");
+        }
+
+        protected void btnSave_Init(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("btnSave_Load");
+            lifeCycleEvent.Add("btnSave_Load");
+        }
+
+        protected void btnSave_PreRender(object sender, EventArgs e)
+        {
+            Debug.WriteLine("btnSave_PreRender");
+            lifeCycleEvent.Add("btnSave_PreRender");
+        }
+
+        protected void ddlCountry_Init(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ddlCountry_Init");
+            lifeCycleEvent.Add("ddlCountry_Init");
+        }
+
+        protected void ddlCountry_Load(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ddlCountry_Load");
+            lifeCycleEvent.Add("ddlCountry_Load");
+        }
+
+        protected void ddlCountry_PreRender(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ddlCountry_PreRender");
+            lifeCycleEvent.Add("ddlCountry_PreRender");
+        }
+    }
+}
